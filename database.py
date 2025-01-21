ee33786ba9fb62402cd910d5bb9d4bf0457452cc
rahulmistry <r26mistry@gmail.com>
2025-01-21 18:32:27 +0530

    change in databse path

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative  import declarative_base
 
-DATABASE_URL = "postgresql://postgres:12345@localhost:5432/media"
+
 engine = create_engine(DATABASE_URL)
 SessionLocal= sessionmaker(autocommit=False, autoflush=True, bind=engine)
 
